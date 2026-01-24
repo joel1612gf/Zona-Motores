41a98f840d48a22e2fb81031f3a58dea2b7cbbda
Joel GF <joel110gf@gmail.com>
2026-01-24 23:33:18 +0000

    hice roll back a unas versiones atras, pero me sale el mismo error de: #

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:342387466922:web:bf7822eb0655ddbeb1e6d3",
   "apiKey": "AIzaSyCL7i8XybBWeCXkATh4aiw9IgIeN0yFaFM",
   "authDomain": "studio-2511212328-1383b.firebaseapp.com",
+  "storageBucket": "studio-2511212328-1383b.appspot.com",
   "measurementId": "",
   "messagingSenderId": "342387466922"
 };
