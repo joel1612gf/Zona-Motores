fcaf5411ad303c640945a599e11537a14cde73a6
Joel GF <joel110gf@gmail.com>
2026-01-25 00:48:36 +0000

    esta es la ruta de acceso al storage gs://studio-2511212328-1383b.fireba

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2511212328-1383b",
   "appId": "1:342387466922:web:bf7822eb0655ddbeb1e6d3",
-  "storageBucket": "studio-2511212328-1383b.appspot.com",
+  "storageBucket": "studio-2511212328-1383b.firebasestorage.app",
   "apiKey": "AIzaSyCL7i8XybBWeCXkATh4aiw9IgIeN0yFaFM",
   "authDomain": "studio-2511212328-1383b.firebaseapp.com",
   "measurementId": "",
