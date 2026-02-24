490fb3fd4c2ca4da16137dc43977367a447ea8f8
Joel GF <joel110gf@gmail.com>
2026-02-24 02:19:48 +0000

    tengo un problema, en la carpeta src firestore y en el config.ts tenemos

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    "projectId": "studio-2511212328-1383b",
-    "appId": "1:342387466922:web:bf7822eb0655ddbeb1e6d3",
-    "storageBucket": "studio-2511212328-1383b.firebasestorage.app",
-    "apiKey": "AIzaSyCL7i8XybBWeCXkATh4aiw9IgIeN0yFaFM",
-    "authDomain": "studio-2511212328-1383b.firebaseapp.com",
-    "measurementId": "",
-    "messagingSenderId": "342387466922"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   };
